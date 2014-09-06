f51e06e7b5e9195bbd82c81f63bb8560896d7aa4
Sebastian Skałacki <skalee@gmail.com>
2014-09-06 19:46:03 +0200

    Instantiate Columns with ::has_secure_token

--- lib/secure_token/model_integrator.rb
+++ lib/secure_token/model_integrator.rb
@@ -2,6 +2,8 @@ module SecureToken
   module ModelIntegrator
 
     def has_secure_token *args
+      options, column_names = args.extract_options!, args
+      column_names.each{ |col| SecureToken::Column.new col, options }
     end
 
   end
--- spec/secure_token_spec.rb
+++ spec/secure_token_spec.rb
@@ -7,6 +7,29 @@ describe SecureToken do
       ActiveRecord::Base.should respond_to :has_secure_token
     end
 
+    it "instantiates Column object for every column covered" do
+      SecureToken::Column.should_receive(:new)
+        .with(:token, some: :options)
+      SecureToken::Column.should_receive(:new)
+        .with(:another_token, some: :options)
+
+      ExampleModel.class_eval do
+        has_secure_token :token, :another_token, some: :options
+      end
+    end
+
+    it "allows defining token columns with different options" do
+      SecureToken::Column.should_receive(:new)
+        .with(:token, some: :options)
+      SecureToken::Column.should_receive(:new)
+        .with(:another_token, other: :options)
+
+      ExampleModel.class_eval do
+        has_secure_token :token, some: :options
+        has_secure_token :another_token, other: :options
+      end
+    end
+
     it "defines method of names concluded from token columns"
   end
 
