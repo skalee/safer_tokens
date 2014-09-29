3fcb3e4690edcd918a4d732bb30ee2b6ff614f35
Sebastian Skałacki <skalee@gmail.com>
2014-09-29 18:08:47 +0200

    Use Module::autoload

--- lib/safer_tokens.rb
+++ lib/safer_tokens.rb
@@ -1,11 +1,17 @@
 require "active_support/core_ext/module/delegation.rb"
 require "active_record"
 
-require "safer_tokens/column"
-require "safer_tokens/cryptography/bcrypt"
-require "safer_tokens/cryptography/cleartext"
-require "safer_tokens/cryptography/scrypt"
-require "safer_tokens/model_integrator"
 require "safer_tokens/version"
+
+module SaferTokens
+  autoload :Column, "safer_tokens/column"
+  autoload :ModelIntegrator, "safer_tokens/model_integrator"
+
+  module Cryptography
+    autoload :BCrypt, "safer_tokens/cryptography/bcrypt"
+    autoload :Cleartext, "safer_tokens/cryptography/cleartext"
+    autoload :SCrypt, "safer_tokens/cryptography/scrypt"
+  end
+end
 
 ActiveRecord::Base.extend SaferTokens::ModelIntegrator
